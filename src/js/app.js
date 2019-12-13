229547c316d785e36acba213f3b4b4188600a033
Николай <37711307+NKhashchanov@users.noreply.github.com>
2019-12-13 13:26:01 +0300

    up

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+export default function destructuring({ special }) {
+  special.forEach((el) => {
+    if (el.description === undefined) {
+      el.description = 'Описание недоступно'; // eslint-disable-line no-param-reassign
+    }
+  });
+  return special;
+}
+
+const data = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+console.log(destructuring(data));
